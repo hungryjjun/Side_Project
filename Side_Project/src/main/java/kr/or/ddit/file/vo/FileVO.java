20e4af3e86ab57da0fa9032a59b18c7734dbb269
young <youngjunlee2@naver.com>
2025-06-02 14:29:19 +0900

    파일첨부 수정

--- Side_Project/src/main/java/kr/or/ddit/file/vo/FileVO.java
+++ Side_Project/src/main/java/kr/or/ddit/file/vo/FileVO.java
@@ -5,13 +5,13 @@ import lombok.Data;
 @Data
 public class FileVO {
 
-	private Long fileId; // No Description
-	private Long noticeId; // No Description
+	private Long fileId; 
+	private Long noticeId; 
 	private String originName; // 원본파일명
 	private String saveName; // DB저장명UUID
 	private String path; // 저장경로
-	private Long fileSize; // No Description
-	private String mimeType; // No Description
+	private Long fileSize; 
+	private String mimeType; 
 	private String status; // 삭제여부
-	private String uploadDt; // No Description
+	private String uploadDt;
 }
